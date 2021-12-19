4b47ab9eb411b23a7a011e45d9d5fb77ded2a6d6
Holy Paul <brijeeta@gmail.com>
2021-12-18 23:06:44 -0500

    added react app with header,navbar,particles,react-typed

---
+++ react-portfolio-app/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import Particles from "react-tsparticles";
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import particlesOptions from "./particles.json";
+import Navbar from './components/NavBar';
+import Header from './components/Header'
+
+function App() {
+    return (
+        <>
+            <Particles options={{
+                particles: {
+                    number: {
+                        density: {
+                            enable: true,
+                            value_area: 900,
+                        },
+                        value: 30,
+                    },
+                    shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 6,
+                            color: "#ffffff"
+                        }
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outMode: "bounce",
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    size: {
+                        random: true,
+                        value: 2,
+                    },
+
+                },
+
+            }} />
+            <Navbar />
+            <Header />
+        </>
+
+    );
+}
+
+export default App;
---
+++ react-portfolio-app/src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import portfoliologo from '../images/logo.PNG'
+
+const navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark text-white bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#home">
+                    <img width="100px" height="90px" src={portfoliologo} />
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{ color: 'white' }} />
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#home">Home <span className="sr-only"></span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#portfolio">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/images/Resume.pdf" download>Resume</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default navbar
